44314fca6709651d66a36cd44bd8059807ea6626
yshuai <stefan_ysh@foxmail.com>
2024-09-12 16:21:12 +0800

    refa: class

--- main.py
+++ main.py
@@ -2,31 +2,37 @@ import tkinter as tk
 from utils.gui import create_gui
 from utils.launch_loading import show_loading_screen
 
+class Application:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.withdraw()  # Hide the main window initially
+
+    def setup(self):
+        show_loading_screen()  # Show loading screen
+
+        self.root.deiconify()  # Show the main window after loading screen
+        self.root.title("Images")
+
+        create_gui(self.root)
+
+        # 设置窗口大小和位置
+        self.root.minsize(600, 400)
+        self.center_window(800, 600)
+
+    def center_window(self, width, height):
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        x = (screen_width - width) // 2
+        y = (screen_height - height) // 2
+        self.root.geometry(f"{width}x{height}+{x}+{y}")
+
+    def run(self):
+        self.root.mainloop()
 
 def main():
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window initially
-
-    show_loading_screen()  # Show loading screen
-
-    root.deiconify()  # Show the main window after loading screen
-    root.title("Images")
-
-    create_gui(root)
-
-    # root 宽高设置
-    root.minsize(600, 400)  # 设置最小宽度为600像素，最小高度为400像素
-    # 居中
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    window_width = 800
-    window_height = 600
-    x = (screen_width - window_width) // 2
-    y = (screen_height - window_height) // 2
-    root.geometry(f"{window_width}x{window_height}+{x}+{y}")
-
-    root.mainloop()
-
+    app = Application()
+    app.setup()
+    app.run()
 
 if __name__ == "__main__":
     main()
